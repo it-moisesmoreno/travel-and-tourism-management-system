3ee6cf6847150e099aa60fea061a015c11a6328b
Moisés Moreno <130410927+it-moisesmoreno@users.noreply.github.com>
2024-02-07 19:28:18 +0000

    Add Init project

---
+++ travel_and_tourism_management_system/src/test/java/com/it_moisesmoreno/travel_and_tourism_management_system/TravelAndTourismManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it_moisesmoreno.travel_and_tourism_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAndTourismManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
